73b956cb1e7e40dce5d8ea9f050ab5fc9e9e2648
Pecola <ephialtes11@gmail.com>
2024-12-11 15:07:40 +1100

    completed general layout and style

---
+++ javaScript.js
@@ -0,0 +1,19 @@
+var num1 = 0; 
+var num2 = 0; 
+var displayNum = 0; 
+
+function addNum(arg1,arg2){
+    return arg1+arg2; 
+}
+
+function subNum(arg1,arg2){
+    return arg1-arg2;
+}
+
+function divNum(arg1,arg2){
+    return arg1/arg2; 
+}
+
+function multNum(arg1,arg2){
+    return arg1*arg2; 
+}
