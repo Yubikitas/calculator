3058594664326810572b2b0987c037db22d7d4c3
Pecola <ephialtes11@gmail.com>
2024-12-12 16:17:42 +1100

    completed basic calculator function and result display

--- javaScript.js
+++ javaScript.js
@@ -1,6 +1,10 @@
-var num1 = 0; 
-var num2 = 0; 
-var displayNum = 0; 
+let num1 = []; 
+let numForArg = 0;
+let numForArg2= 0;  
+var displayNum = 0;
+var operand = ""
+
+num1[0] =0; 
 
 function addNum(arg1,arg2){
     return arg1+arg2; 
@@ -17,3 +21,129 @@ function divNum(arg1,arg2){
 function multNum(arg1,arg2){
     return arg1*arg2; 
 }
+
+function operate(arg1,arg2,operand){
+
+    input1 = Number(arg1);  
+    input2 = Number(arg2);  
+
+ 
+
+
+    if (operand=="plus"){
+        return addNum(input1,input2)
+    } 
+    else if (operand=="minus"){
+        return subNum(input1,input2)
+    }
+    else if (operand =="divide"){
+        return divNum(input1,input2)
+    }
+    else if (operand=="multiply"){
+        return multNum(input1,input2)
+    }
+}
+
+
+let displayNumber = document.querySelector("#calcDisplay"); 
+displayNumber.textContent = 0;  
+
+let getNum = document.querySelector("#calcNumberButtons"); 
+getNum.addEventListener("click",(e)=>{
+    let target = e.target; 
+    if (num1[0]===0){
+        num1.splice(0,1); 
+    }
+
+        if (target.id=="seven"&& num1.length<=9){
+                num1.push(7); 
+                displayNumber.textContent = num1.join(""); 
+            } 
+        else if (target.id=="eight"&& num1.length<=9){
+                num1.push(8); 
+                displayNumber.textContent = num1.join(""); 
+            }
+        else if (target.id=="nine"&& num1.length<=9){
+                num1.push(9); 
+                displayNumber.textContent = num1.join(""); 
+            }
+        else if (target.id=="four"&& num1.length<=9){
+                num1.push(4); 
+                displayNumber.textContent = num1.join(""); 
+            }
+        else if (target.id=="five"&& num1.length<=9){
+                num1.push(5); 
+                displayNumber.textContent = num1.join(""); 
+            }   
+        else if (target.id=="six"&& num1.length<=9){
+                num1.push(6); 
+                displayNumber.textContent = num1.join(""); 
+            }   
+        else if (target.id=="one"&& num1.length<=9){
+                num1.push(1); 
+                displayNumber.textContent = num1.join(""); 
+            }   
+        else if (target.id=="two"&& num1.length<=9){
+                num1.push(2); 
+                displayNumber.textContent = num1.join(""); 
+            }   
+        else if (target.id=="three"&& num1.length<=9){
+                num1.push(3); 
+                displayNumber.textContent = num1.join(""); 
+            }
+        else if (target.id=="zero"&& num1.length<=9){
+                num1.push(0); 
+                displayNumber.textContent = num1.join(""); 
+            }
+        else if (target.id=="doubleZero"&& num1.length<=8){
+                num1.push(0);
+                num1.push(0);  
+                displayNumber.textContent = num1.join(""); 
+            }
+        }); 
+
+    let clearNum = document.querySelector("#clear"); 
+    clearNum.addEventListener("click",(e)=>{
+        displayNumber.textContent = "0"; 
+        num1.splice(0,num1.length); 
+    }); 
+
+    let getOperand = document.querySelector("#calcOperateButtons"); 
+    getOperand.addEventListener("click",(e)=>{
+        let target = e.target; 
+        switch(target.id){
+            case "plus": 
+            operand = "plus"; 
+            numForArg = num1.slice().join(""); 
+            num1.splice(0,num1.length);
+            break; 
+            
+            case "minus": 
+            operand = "minus";
+            numForArg = num1.slice().join(""); 
+            num1.splice(0,num1.length);
+            break;  
+
+            case "multiply": 
+            operand = "multiply"; 
+            numForArg = num1.slice().join(""); 
+            num1.splice(0,num1.length);
+            break; 
+
+            case "divide": 
+            operand = "divide"; 
+            numForArg = num1.slice().join("");  
+            num1.splice(0,num1.length);
+            break; 
+
+            case"equal": 
+            numForArg2 = num1.slice().join(""); 
+            displayNum = operate(numForArg,numForArg2,operand); 
+            console.log(displayNum); 
+            displayNumber.textContent = displayNum; 
+
+    
+        }}); 
+
+            
+    
